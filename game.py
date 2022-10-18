a91ab70866ffb08f6519d7682d36b62df4133564
dmoynihan25 <m254551@usna.edu>
2022-10-18 09:27:25 -0400

    screen size adjustment
    this made the ship visible on my laptop

--- game.py
+++ game.py
@@ -14,8 +14,6 @@ class AlienInvasion:
 
         self.screen = pygame.display.set_mode(
             (self.settings.screen_width, self.settings.screen_height))
-
-        self.screen = pygame.display.set_mode((1200,800))
         pygame.display.set_caption("Alien Invasion")
 
         self.ship = Ship(self)
