f1619ab64c87b8998e56c47947e94f7cbb57978a
dmoynihan25 <m254551@usna.edu>
2022-11-01 19:41:53 -0400

    finished 12-4

--- homework1/prob 12-4.py
+++ homework1/prob 12-4.py
@@ -14,15 +14,37 @@ class Alien_Invasion():
 
         self.ship = Ship(self)
 
+
     def run_game(self):
         while True:
             self._check_events()
+            self.ship.update()
             self._update_screen()
 
     def _check_events(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = True
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = True
+                elif event.key == pygame.K_UP:
+                    self.ship.moving_up = True
+                elif event.key == pygame.K_DOWN:
+                    self.ship.moving_down = True
+
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = False
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = False
+                elif event.key == pygame.K_UP:
+                    self.ship.moving_up = False
+                elif event.key == pygame.K_DOWN:
+                    self.ship.moving_down = False
+
 
     def _update_screen(self):
         self.screen.fill(self.bg_color)
@@ -39,6 +61,29 @@ class Ship:
         self.rect = self.image.get_rect()
 
         self.rect.center = self.screen_rect.center
+
+        self.x = float(self.rect.x)
+        self.y = float(self.rect.y)
+
+        self.ship_speed = 1.5
+
+        self.moving_right = False
+        self.moving_left = False
+        self.moving_up = False
+        self.moving_down = False
+
+    def update(self):
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.x += self.ship_speed
+        if self.moving_left and self.rect.left > 0 :
+            self.x -= self.ship_speed
+        if self.moving_up and self.rect.top < self.screen_rect.top:
+            self.y += self.ship_speed
+        if self.moving_down and self.rect.bottom > 0:
+            self.y -= self.ship_speed
+        self.rect.x = self.x
+        self.rect.y = self.y
+
     def blitme(self):
         self.screen.blit(self.image, self.rect)
 
